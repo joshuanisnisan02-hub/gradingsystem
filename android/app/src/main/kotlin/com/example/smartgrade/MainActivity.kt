2c58e9d4e436b89896913584d32952dbd9d36cfa
joshuanisnisan02-hub <joshua.nisnisan02@gmail.com>
2026-08-25 15:38:57 +0800

    Fix Flutter checks

---
+++ android/app/src/main/kotlin/com/example/smartgrade/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smartgrade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../core/supabase_client.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
